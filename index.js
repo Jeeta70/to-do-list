3194cba5aa8dc612ebc8eed642fb273da17fbd68
Ajeet Singh <ajrana70@gmail.com>
2022-11-28 18:56:27 +0530

    add hover effect in icon

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 //input field wrapper selectors
-const lis = document.querySelectorAll("ul > li");
-const icons = document.querySelectorAll("ul > li .fa-circle-xmark");
+const totalTask = document.getElementById("total-task");
 const plusIcon = document.getElementsByClassName("fa-plus")[0];
 const inputField = document.querySelector('input[type="text"]');
 const testListTag = document.getElementById("task-list");
 
 const tasksList = [];
-let currentTask = "";
 
-
-for (let i = 0; i < lis.length; i++) {
-  let element = lis[i];
-  element.addEventListener("mouseover", function () {
-    icons[i].style.visibility = "visible";
-  });
-  element.addEventListener("mouseout", function () {
-    icons[i].style.visibility = "hidden";
-  });
+function handleHoverIcon() {
+  // console.log(testListTag);
+  const lis = document.querySelectorAll("ul > li");
+  const icons = document.querySelectorAll("ul > li .fa-circle-xmark");
+  for (let i = 0; i < lis.length; i++) {
+    let element = lis[i];
+    element.addEventListener("mouseover", function () {
+      icons[i].style.visibility = "visible";
+    });
+    element.addEventListener("mouseout", function () {
+      icons[i].style.visibility = "hidden";
+    });
+  }
 }
 
-
 function updateValue(e) {
-  if (e.target.value) {
-    currentTask = `${e.target.value}`;
-    plusIcon.style.visibility = "visible";
-  } else {
-    plusIcon.style.visibility = "hidden";
+  if (e.key === "Enter" && e.target.value !== "") {
+    console.log(e.target.value);
+    console.log("Enter key press");
+    tasksList.push(e.target.value);
+    //create li tag
+    const li = document.createElement("li");
+    //create left icon
+    const leftIcon = document.createElement("i");
+    leftIcon.classList.add("fa-regular", "fa-circle", "text-center");
+    //create span
+    const span = document.createElement("span");
+    span.innerText = e.target.value;
+
+    //create right icon
+    const rightIcon = document.createElement("i");
+    rightIcon.classList.add(
+      "fa-regular",
+      "fa-circle-xmark",
+      "text-center",
+      "hide"
+    );
+    li.appendChild(leftIcon);
+    li.appendChild(span);
+    li.appendChild(rightIcon);
+    testListTag.appendChild(li);
+    e.target.value = "";
+    handleHoverIcon();
   }
 }
 
@@ -58,6 +81,4 @@ function addTask(e) {
   }
 }
 
-inputField.addEventListener("input", updateValue);
-inputField.addEventListener("keypress", addTask);
-
+inputField.addEventListener("keypress", updateValue);
